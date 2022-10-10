6f99e7d586f711e5583e1291e999c7de5a19e9c8
Simon Balleux <simoncestmoi@hotmail.fr>
2022-10-10 12:39:56 +0200

    creation namespace autoloader avec composer

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
 // le fichier est utilisé pour tte les pages et c'est lui qui va rediriger à chaque fois, il verra la requete demandée et redirigera sur le bon fichier en fonction
+// Charger mon autloader
+require '../vendor/autoload.php';
 
-echo 'Salut';
+
+// Classe qui represente mon application
+$app = new \Framework\App();
+
+$app->run();
---
+++ src/Framework/App.php
@@ -0,0 +1,10 @@
+<?php
+
+// Definir mon namespace
+namespace Framework;
+
+class App {
+    public function run() {
+        echo 'salut ca marche !';
+    }
+}
