23b1b5599d9bd72628a1d7126bf8347d75d9ff8d
Simon Balleux <simoncestmoi@hotmail.fr>
2022-10-10 13:04:41 +0200

    Installation de psr7 / guzzle, pour objets Request et Response

--- public/index.php
+++ public/index.php
@@ -1,11 +1,15 @@
 <?php
 
 // le fichier est utilisé pour tte les pages et c'est lui qui va rediriger à chaque fois, il verra la requete demandée et redirigera sur le bon fichier en fonction
+
 // Charger mon autloader
 require '../vendor/autoload.php';
 
 
-// Classe qui represente mon application
+// Classe qui represente mon application, initialisation de App
 $app = new \Framework\App();
 
-$app->run();
+
+
+// stocker ma réponse et generer la requete avec Guzzle
+$response = $app->run(\GuzzleHttp\Psr7\ServerRequest::fromGlobals());
--- src/Framework/App.php
+++ src/Framework/App.php
@@ -1,10 +1,34 @@
 <?php
 
-// Definir mon namespace
+// ma classe se situe dans le namespace Framework
 namespace Framework;
 
+// Objet Response
+use GuzzleHttp\Psr7\Response;
+// Librairie pour mes methods Request et Response
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
 class App {
-    public function run() {
-        echo 'salut ca marche !';
+
+    // Requete en param et response en sortie de la fonction
+    public function run(ServerRequestInterface $request): ResponseInterface {
+        // recuperer mon url, sur ma requete jai ma methode getUri et dans cette methode je n'est besoin que du chemin
+        $uri = $request->getUri()->getPath();
+
+        // Simon mon url n'est pas vide et fini par un / alors je fait une redirection
+        if (!empty($uri) && $uri[-1] === "/") {
+            // Crée une instance de Response et lui appliquer mes methods
+            $response = (new Response())
+            ->withStatus(301)
+            ->withHeader('Location', substr($uri, 0, -1));
+
+            return $response;
+        }
+
+        // sinon
+        $response = new Response();
+        $response->getBody()->write('Bonjour');
+        return $response;
     }
 }
