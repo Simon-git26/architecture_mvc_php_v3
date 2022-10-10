ab9a9628fde5d47de083e70e6c272d2c381f6a27
Simon Balleux <simoncestmoi@hotmail.fr>
2022-10-10 12:29:00 +0200

    Création index.php

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+// le fichier est utilisé pour tte les pages et c'est lui qui va rediriger à chaque fois, il verra la requete demandée et redirigera sur le bon fichier en fonction
+
+echo 'Salut';
